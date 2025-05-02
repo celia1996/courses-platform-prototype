b56a63a2b012fd5a516b03afc52bae06ce838c5e
Celia Navarro Pérez <celiinavarroperez@gmail.com>
2025-05-02 13:02:23 +0200

    Create course prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import { fetchSchedule, ClassItem } from "./api/schedule";
+import { CourseTile } from "./components/CourseTile";
+import { SaveButton } from "./components/SaveButton";
 function App() {
-  const [count, setCount] = useState(0)
+  const courseId = 1234;
+  const [classes, setClasses] = useState<ClassItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+  const handleEnroll = () => {
+    console.log("the user wants to enroll");
+  };
+  useEffect(() => {
+    fetchSchedule(courseId)
+      .then((data) => setClasses(data))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <div>Loading schedule…</div>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="content-container stack-24">
+      <label className="text-title-large text-neutral-0">Course Dates</label>
+      <div className="stack-24" role="radiogroup" aria-label="Course Dates">
+        {classes.map((cls) => (
+          <CourseTile
+            key={cls.id}
+            item={cls}
+            selectedId={selectedId}
+            onSelect={setSelectedId}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <button
+        className="enroll-button text-heading-small text-neutral-100"
+        onClick={handleEnroll}
+      >
+        Enroll in Course
+      </button>
+      <SaveButton />
+    </div>
+  );
 }
 
-export default App
+export default App;
